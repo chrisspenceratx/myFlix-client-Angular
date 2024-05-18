d7d941b9455cfc80289bb6cf69c06d67f504c039
chrisspenceratx <chrisspenceratx@gmail.com>
2024-05-18 08:01:54 -0500

    implements login

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -2,6 +2,7 @@ import { FetchApiDataService } from '../fetch-api-data.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router'; // Import Router
 
 @Component({
   selector: 'app-user-login-form',
@@ -13,9 +14,10 @@ export class UserLoginFormComponent implements OnInit {
   @Input() userData = { Username: '', Password: '' };
 
   constructor(
-    public FetchApiDataService: FetchApiDataService,
+    public fetchApiDataService: FetchApiDataService, // Rename FetchApiDataService to follow naming convention
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar
+    public snackBar: MatSnackBar,
+    private router: Router // Inject Router
   ) { }
 
 
@@ -26,7 +28,7 @@ export class UserLoginFormComponent implements OnInit {
    * Function responsible for sending the form inputs to the backend
    */
   loginUser(): void {
-    this.FetchApiDataService.userLogin(this.userData).subscribe((result) => {
+    this.fetchApiDataService.userLogin(this.userData).subscribe((result) => {
       // Logic for a successful user login
       console.log(result);
       localStorage.setItem('user', JSON.stringify(result.user));
@@ -36,6 +38,9 @@ export class UserLoginFormComponent implements OnInit {
       this.snackBar.open('user logged in successfully', 'OK', {
         duration: 2000
       });
+
+      // Navigate to 'movies' route on successful login
+      this.router.navigate(['movies']);
     }, (result) => {
       this.snackBar.open('login failed', 'OK', {
         duration: 2000
